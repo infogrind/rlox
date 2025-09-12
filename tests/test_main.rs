9e7a04a253071f2d7aa8924c47b2625df8cfa2cd
Marius Kleiner <kleiner@gmail.com>
2025-09-12 17:40:49 +0200

    Add e2e test for main app

---
+++ tests/test_main.rs
@@ -0,0 +1,25 @@
+use googletest::prelude::*;
+use std::io::Write;
+use std::process::{Command, Stdio};
+
+const APP_NAME: &str = "target/debug/rlox";
+
+#[gtest]
+fn test_empty() {
+    let mut child = Command::new(APP_NAME)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Could not start child command");
+    if let Some(stdin) = child.stdin.as_mut() {
+        writeln!(stdin, "Hund").expect("Error writing to child process");
+        // stdin is dropped here, sending an EOF. Also, dropping implicitly flushes.
+    }
+
+    let output = child.wait_with_output().expect("Error waiting for child");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    expect_that!(
+        stdout.lines().collect::<Vec<&str>>(),
+        elements_are!(&"> You wrote: Hund", &"> ", &"Ok, bye!")
+    );
+}
