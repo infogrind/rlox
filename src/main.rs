2d048cb2e9168b09c92dbebb7ddee0472697bcbb
Marius Kleiner <kleiner@gmail.com>
2025-09-12 16:49:15 +0200

    Implemented loop to read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    loop {
+        buffer.clear();
+        print!("> ");
+        io::stdout().flush().unwrap();
+        match io::stdin().read_line(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    println!("\nOk, bye!");
+                    break;
+                } else {
+                    println!("You wrote: {}", buffer.trim());
+                }
+            }
+            Err(e) => {
+                eprintln!("Error: {e:?}");
+                break;
+            }
+        }
+    }
 }
