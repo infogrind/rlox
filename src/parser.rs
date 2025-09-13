244759723079c86e07e19c15c156b52609574a2c
Marius Kleiner <kleiner@gmail.com>
2025-09-13 14:04:52 +0200

    Parse addition and subtraction

--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,55 @@
 use crate::syntax::Expression::{self, *};
 use crate::tokens::Token::{self, *};
+use std::iter::Peekable;
 
 pub fn parse_expression<I>(tokens: &mut I) -> Option<Expression>
 where
     I: Iterator<Item = Token>,
 {
     let mut p = tokens.peekable();
-    let c = p.peek()?;
-    match c {
-        NumberToken(i) => Some(Number(*i)),
-        t => unimplemented!("Cannot parse token {:?} yet.", t),
+    let result = match p.peek() {
+        Some(_) => Some(parse_term(&mut p)),
+        None => None,
+    };
+    // Make sure no tokens follow.
+    match p.peek() {
+        Some(t) => panic!("Unexpected token {:?} after term.", t),
+        None => result,
+    }
+}
+
+fn parse_term<I>(p: &mut Peekable<I>) -> Expression
+where
+    I: Iterator<Item = Token>,
+{
+    let mut lhs = parse_primary(p);
+    loop {
+        match p.peek() {
+            // End of input
+            Some(Plus) => {
+                p.next();
+                lhs = Add(Box::new(lhs), Box::new(parse_primary(p)));
+            }
+            Some(Minus) => {
+                p.next();
+                lhs = Sub(Box::new(lhs), Box::new(parse_primary(p)));
+            }
+            // End of production, just return what we have.
+            Some(_) => break lhs,
+            // End of input, just return what we have.
+            None => break lhs,
+        }
+    }
+}
+
+fn parse_primary<I>(p: &mut Peekable<I>) -> Expression
+where
+    I: Iterator<Item = Token>,
+{
+    match p.next() {
+        Some(NumberToken(i)) => Number(i),
+        None => panic!("Unexpected end of expression"),
+        Some(t) => panic!("Unexpected token while parsing primary: {:?}", t),
     }
 }
 
@@ -27,5 +67,35 @@ mod tests {
     #[gtest]
     fn parse_number() {
         expect_that!(p("123"), eq(&Some(Number(123))));
+    }
+
+    #[gtest]
+    fn parse_addition() {
+        expect_that!(
+            p("1+2"),
+            eq(&Some(Add(Box::new(Number(1)), Box::new(Number(2)))))
+        )
+    }
+
+    #[gtest]
+    fn parse_addition_three_terms() {
+        expect_that!(
+            p("1+2+3"),
+            eq(&Some(Add(
+                Box::new(Add(Box::new(Number(1)), Box::new(Number(2)))),
+                Box::new(Number(3))
+            )))
+        )
+    }
+
+    #[gtest]
+    fn parse_subtraction_three_terms() {
+        expect_that!(
+            p("1-2-3"),
+            eq(&Some(Sub(
+                Box::new(Sub(Box::new(Number(1)), Box::new(Number(2)))),
+                Box::new(Number(3))
+            )))
+        )
     }
 }
